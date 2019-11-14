6a5a27a49738b5c7b02bd4f2c6230a84c133fa9d
Eugene Naryshkin <allainmoyo@gmail.com>
2019-11-14 15:35:42 +0200

    add roles

--- homework.sh
+++ homework.sh
@@ -1,26 +1,58 @@
 #!/bin/sh
-# install nginx
-ansible-playbook -i hosts nginx.yaml --tags install_nginx > 01install_nginx.log
 
+# read -p "Create log files? (y) > " addlog
+
+# install nginx
+if [[ $1 == 'y' ]] || [[ $1 == 'yes' ]]
+then
+  echo "stage01: Install nginx"
+  ansible-playbook -i hosts nginx.yaml --tags install_nginx > 01install_nginx.log
+  echo "stage02: Start nginx service"
+  ansible-playbook -i hosts nginx.yaml --tags start_nginx > 02start_nginx.log
+  echo "stage03: Add nginx service as autorun"
+  ansible-playbook -i hosts nginx.yaml --tags enable_nginx > 03enable_nginx.log
+  echo "stage04: Disable nginx service"
+  ansible-playbook -i hosts nginx.yaml --tags disable_nginx > 04disable_nginx.log
+  echo "stage05: Stop nginx service"
+  ansible-playbook -i hosts nginx.yaml --tags stop_nginx > 05stop_nginx.log
+  echo "stage06: Uninstall nginx"
+  ansible-playbook -i hosts nginx.yaml --tags uninstall_nginx > 06uninstall_nginx.log
+  echo "stage07: Install zip, unzip, gzip, nano"
+  ansible-playbook -i hosts tools.yaml --tags install > 07install_tools.log
+  echo "stage08: Uninstall unzip, nano"
+  ansible-playbook -i hosts tools.yaml --tags uninstall > 08uninstall_tools.log
+
+else
+  echo "yes parameter not found. No logs will be created." 
+  
+  echo "stage01: Install nginx"
+  ansible-playbook -i hosts nginx.yaml --tags install_nginx
 # start nginx service
-ansible-playbook -i hosts nginx.yaml --tags start_nginx > 02start_nginx.log
+  echo "stage02: Start nginx service"
+  ansible-playbook -i hosts nginx.yaml --tags start_nginx
 
 # enable nginx startup service 
-ansible-playbook -i hosts nginx.yaml --tags enable_nginx > 03enable_nginx.log
+  echo "stage03: Add nginx service as autorun"
+  ansible-playbook -i hosts nginx.yaml --tags enable_nginx
 
 # disabe nginx startup service
-ansible-playbook -i hosts nginx.yaml --tags disable_nginx > 04disable_nginx.log
+  echo "stage04: Disable nginx service"
+  ansible-playbook -i hosts nginx.yaml --tags disable_nginx
 
 # stop nginx service
-ansible-playbook -i hosts nginx.yaml --tags stop_nginx > 05stop_nginx.log
+  echo "stage05: Stop nginx service"
+  ansible-playbook -i hosts nginx.yaml --tags stop_nginx
 
 #uninstall nginx
-ansible-playbook -i hosts nginx.yaml --tags uninstall_nginx > 06uninstall_nginx.log
+  echo "stage06: Uninstall nginx"
+  ansible-playbook -i hosts nginx.yaml --tags uninstall_nginx
 
 # --------------------------------------------------------------------------
-# install tools
-ansible-playbook -i hosts tools.yaml --tags install > 07install_tools.log
+  # install tools
+  echo "stage07: Install zip, unzip, gzip, nano"
+  ansible-playbook -i hosts tools.yaml --tags install
 
-#uninstall tools
-ansible-playbook -i hosts tools.yaml --tags uninstall > 08uninstall_tools.log
-
+  #uninstall tools
+  echo "stage08: Uninstall unzip, nano"
+  ansible-playbook -i hosts tools.yaml --tags uninstall
+fi
